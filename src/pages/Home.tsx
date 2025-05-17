17b7d4115a62c1b3bc1924d0b2d208e288f86e5c
Fietstopa <bmisko984@gmail.com>
2025-05-17 19:52:40 +0200

    df

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
 
@@ -6,55 +5,35 @@ import ImageSwitcher from "../components/imageSwitch";
 import PromoPageOne from "../screens/promoPageOne";
 import IgCarousel from "../components/igCarousel";
 
-import imageUrl1 from "../../public/images/interior/2.jpg";
-import imageUrl2 from "../../public/images/interior/5.jpg";
-import imageUrl3 from "../../public/images/interior/11.jpg";
-
-import imageUrlVertical1 from "../../public/images/vertical/3.jpg";
-import imageUrlVertical2 from "../../public/images/vertical/22.jpg";
-import imageUrlVertical3 from "../../public/images/vertical/2.jpg";
-import { image } from "framer-motion/client";
+import imageUrl1 from "../../public/images/room1/1.jpg";
+import imageUrl2 from "../../public/images/room1/2.jpg";
+import imageUrl3 from "../../public/images/room1/3.jpg";
+import imageUrlV1 from "../../public/images/room1/6.jpg";
+import imageUrlV2 from "../../public/images/room1/7.jpg";
+import imageUrlV3 from "../../public/images/room1/8.jpg";
 
 const Home = () => {
   const { t } = useTranslation();
 
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-
-    const handleResize = (e: MediaQueryListEvent) => {
-      setIsMobile(e.matches);
-    };
-
-    // Initial check
-    setIsMobile(mediaQuery.matches);
-
-    // Add listener
-    mediaQuery.addEventListener("change", handleResize);
-
-    // Cleanup
-    return () => mediaQuery.removeEventListener("change", handleResize);
-  }, []);
-
-  const image1 = isMobile ? imageUrlVertical1 : imageUrl1;
-  const image2 = isMobile ? imageUrlVertical2 : imageUrl2;
-  const image3 = isMobile ? imageUrlVertical3 : imageUrl3;
-
   return (
     <div className="bg-gray-100">
+      <Helmet>
+        <title>{t("home.title")}</title>
+      </Helmet>
+
       <ImageSwitcher
-        imageDesktop1={image1}
-        imageDesktop2={image2}
-        imageDesktop3={image3}
-        imageMobile1={imageUrlVertical1}
-        imageMobile2={imageUrlVertical2}
-        imageMobile3={imageUrlVertical3}
+        imageDesktop1={imageUrl1}
+        imageDesktop2={imageUrl2}
+        imageDesktop3={imageUrl3}
+        imageMobile1={imageUrlV1}
+        imageMobile2={imageUrlV2}
+        imageMobile3={imageUrlV3}
         caption={t("home.caption")}
         undertext={t("home.secondaryCaption")}
         description={t("home.descriptionText")}
         btnText={t("home.reservation")}
       />
+
       <PromoPageOne />
       <IgCarousel />
     </div>
